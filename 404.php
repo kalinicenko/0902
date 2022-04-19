1abd4f0e5ccde406be58c8d0f32647c177576bf0
kalinicenko <102030651+kalinicenko@users.noreply.github.com>
2022-04-19 16:00:40 +0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="media.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Продажа запчастей в АВТОКОМПАНИИ ЗА РУЛЁМ №1 В ПЕРМИ И ПЕРМСКОМ КРАЕ</title>
+    <meta name="description" content="АВТОКОМПАНИЯ ЗА РУЛЁМ №1 В ПЕРМИ И ПЕРМСКОМ КРАЕ занимается подбором, заказом, установкой запчастей для вашего автомобиля">
+</head>
+<body>
+    <header>
+        <menu>
+            <img class="logotip" src="img/logoza1.png">
+            <div class="nazvania">
+            <a href="index.html">Главная</a>
+            <a href="#">Адреса</a>
+            <a href="kompani.html">О компании</a>
+            <a href="news.html">Новости</a></div>
+        </menu>
+        <div class="menu">
+            <img class="logotip" src="img/logoza1.png">
+            <div class="dropdown" style="float:right;">
+            <button class="dropbtn"><img src="img/menu.png"></button>
+            <div class="dropdown-content">
+                <a href="index.html">Главная</a>
+                <a href="#">Адреса</a>
+                <a href="kompani.html">О компании</a>
+                <a href="news.html">Новости</a></div>
+            </div>
+          </div>
+        </div>
+        <p class="slogan">АВТОКОМПАНИЯ <span> ЗА РУЛЁМ<br>№1</span> В ПЕРМИ И ПЕРМСКОМ КРАЕ</p>
+        <div class="button">
+            <button class="odin">Личный кабинет</button>
+            <button class="dva">Подробнее</button>
+        </div>
+    </header>
+    <content>
+        <div class="stroka_2">
+            <h1>404</h1>
+            <h2>Просим прощения, но страница, которую вы искали - не нашлась!</h2>
+        </div>   
+        <div class="stroka_3">
+            <p>Возможно это произошло из-за опечатки при вводе адреса страницы или потому что
+                страница усторела и была удалина. В любом случае, мы очень сожалеем и предлагаем воспользоваться
+                формой поиска, надеемся, страница найдётся.</p>
+            <div class="knopki1">
+                <div class="naglavnuy"><button><a href="index.html">Перейти на главную</a></button></div>
+            </div>
+        </div>
+    </content>
+    <footer>
+        <div class="footer"><img class="logotip1" src="img/logoza1.png"></div>
+            <div class="footer1">
+                <p>г. Пермь, ул. Вавилова, 365</p>
+                <p>+7 919 586 2568</p>
+                <a href="#"><img src="img/logo-whatsapp-png-blanco-5-original.png"></a>
+                <a href="#"><img src="img/pngwing.com(5).png"></a>
+                <a href="#"><img src="img/kisspng-vkontakte-social-networking-service-computer-icons-5b15a3aa7aedc3.7435849915281448105035.png"></a>
+                <a href="#"><img src="img/pngegg(3).png"></a>
+            </div>
+                <div class="footers"><p>Продажа контрактных автозапчастей<br>для японских автомобилей и сервис</p></div>
+    </footer>  
+</body>
+</html>
